3640fc7f17e732ef760daed4a78da26ebc55d7e8
lquastana <laurent.quastana@gmail.com>
2019-09-08 00:08:22 +0200

    First Chapter

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 import feedparser
 app = Flask(__name__)
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
@@ -9,17 +9,9 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 @app.route("/")
 @app.route("/<publication>")
 def get_news(publication="bbc"):
-    feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
-    return """<html>
-    <body>
-    <h1>Headlines </h1>
-    <b>{0}</b> </ br>
-    <i>{1}</i> </ br>
-    <p>{2}</p> </ br>
-    </body>
-    </html>""".format(first_article.get("title"), first_article.
-    get("published"), first_article.get("summary"))
+ feed = feedparser.parse(RSS_FEEDS[publication])
+ first_article = feed['entries'][0]
+ return render_template("home.html",title=first_article.get("title"),published=first_article.get("published"),summary=first_article.get("summary"))
 
 
 if __name__ == '__main__':
